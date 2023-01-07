06fdafebe4298c6eb731f3277613e3d7b1ef045a
Eskil Heyn Olsen <eskil@eskil.org>
2023-01-06 19:18:04 -0800

    More complex examples

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 Playground sample of using [luerl](https://github.com/rvirding/luerl).
 
-This example shows how to call lua script code from elixir. Next up
-I'll try and add a custom library function to lua.
+See this excellent post by Kevin Hoffman on how to use luerl in elixir; [Hosting a Lua Script inside an Elixir GenServer for Fun and Games](https://kevinhoffman.medium.com/hosting-a-lua-script-inside-an-elixir-genserver-for-fun-and-games-2c0662660007)
--- lib/luerlex_cli.ex
+++ lib/luerlex_cli.ex
@@ -21,17 +21,33 @@ defmodule LuerlEx.CLI do
     Logger.info("Configuration: #{inspect cli_args}")
 
 
+    # Start a lua state that we'll pass around to all lua calls.
     lua_state = :luerl.init()
-    # lua_state =
-    #   for {name, fun} <- lua_function_table() do
-    #     :luerl.set_table(name, fun, lua_state)
-    #   end
-    lua_state = :luerl.set_table(["hello_world"], &hello_world/2, lua_state)
+
+    # Register some functions that we'll allow lua scripts to call
+    lua_state = Enum.reduce(lua_function_table(), lua_state, fn {name, fun}, lua_state ->
+      :luerl.set_table(name, fun, lua_state)
+    end)
+
+    # Parse the lua script into a chunk
     {:ok, chunk, lua_state} = :luerl.load(lua_script(), lua_state)
+
+    # Send a message in 5 seconds to test wait_for
+    parent = self()
+    spawn(fn ->
+      Process.sleep(5000)
+      send(parent, {:msg, "big whoop"})
+    end)
+
+    # Execute the chunk
     {result, _lua_state} = :luerl.do(chunk, lua_state)
-    Logger.info("result: #{inspect result}")
+
+    # Print the result of the script
+    IO.puts("result: #{inspect result}")
   end
 
+  # You can ignore the optimus arg parsing. This is just my boilerplate escript
+  # code since scripts often suddenly need an argument or two.
   def parse_argv(argv) do
     Optimus.new!(
       name: "luerlex",
@@ -62,6 +78,9 @@ defmodule LuerlEx.CLI do
     |> Optimus.parse!(argv)
   end
 
+  @doc """
+  This function simply returns the script we want to execute.
+  """
   def lua_script() do
     """
     print("Hello Zak")
@@ -96,20 +115,105 @@ defmodule LuerlEx.CLI do
     add_to_my_dict("Chuck", "is a plant.")
     print_dict(my_dict)
 
+    print("")
     print("Calling elixir functions")
-    s = hello_world("tuna")
-    print("Result = "..s)
 
-    return 12, "hello"
+    s = hello_world("tuna")
+    print("hello_world = "..s)
+
+    x, y = echo(10, 3)
+    print("echo = "..x..","..y)
+
+    r = adder(10, 3)
+    print("adder = "..r)
+
+    sleep_for(2)
+
+    print("Going to wait 10 seconds for a message...")
+    msg = wait_for(10)
+    print("message = "..msg)
+
+    print("Done")
+
+    return 3, "heads"
     """
   end
 
+  @doc """
+  This function returns a list of function (name and fn pairs) that we
+  want to register.
+
+  Note that the function name is a `["list"]`. This is neccesary when
+  passing it to luerl.
+  """
   def lua_function_table() do
-    [{"hello_world", &hello_world/2}]
+    [
+      {["hello_world"], &hello_world/2},
+      {["adder"], &adder/2},
+      {["echo"], &echo/2},
+      {["sleep_for"], &sleep_for/2},
+      {["wait_for"], &wait_for/2},
+    ]
   end
 
-  def hello_world(str, lua_state) do
-    IO.puts "Hello world and #{str}"
+  @doc """
+  One example function that is called from lua.
+
+  Functions exposed to lua receive the arguments as a list (`args`)
+  and the lua state.
+
+  They must return a tuple of `{[r1, r2...], lua_state}`
+
+  Lua allows binding of multiple result values, so you can call them as
+  ```
+  r1, r2 = elixir_func(arg)
+  ```
+  """
+  def hello_world(args, lua_state) do
+    IO.puts "Hello world and #{inspect args, pretty: true}"
     {["tuna head"], lua_state}
+  end
+
+  @doc """
+  Another example that just echos the args back. This tests binding
+  to mulitple return values.
+
+  ```
+  r1, r2 = echo(1, 2)
+  ```
+  """
+  def echo(args, lua_state) do
+    {args, lua_state}
+  end
+
+  @doc """
+  Trivial added function, performs an operation on the input
+  and returns the result.
+  """
+  def adder([a, b] = _args, lua_state) do
+    {[a+b], lua_state}
+  end
+
+  @doc """
+  Pause the lua script for a while.
+  """
+  def sleep_for([seconds], lua_state) do
+    Process.sleep(1_000 * seconds)
+    {[], lua_state}
+  end
+
+  @doc """
+  Pause the lua script for a while or receive
+  a message and return the contents to lua.
+
+  This demonstrates coordinating lua scripts with elixir but waiting
+  for messages.
+  """
+  def wait_for([seconds], lua_state) do
+    receive do
+      {:msg, msg} -> {[msg], lua_state}
+    after
+      1_000 * seconds -> {["three headed monkey"], lua_state}
+    end
   end
 end
